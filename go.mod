cb505845db4b8edbf2cdab9da64cf82070e7dccd
Yuki Ito <mrno110y@gmail.com>
2020-01-12 02:13:10 +0000

    tmp

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/110y/glm.nvim
 go 1.13
 
 require (
-	github.com/110y/glm v0.0.0-20191102113605-fb63d965cf53
-	github.com/neovim/go-client v1.0.0
+	github.com/110y/glm v0.0.0-20200110081151-a2d1b0848bc3
+	github.com/neovim/go-client v1.0.1
 )
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/110y/glm/pkg/glm"
@@ -26,6 +29,8 @@ func runGLMWorker(args []string) (string, error) {
 			// return "", errors.New("invalid number of arguments")
 		}
 
+		glmfile := args[0]
+
 		duration := args[1]
 		d, err := time.ParseDuration(duration)
 		if err != nil {
@@ -34,16 +39,49 @@ func runGLMWorker(args []string) (string, error) {
 		}
 
 		for {
-			<-time.After(d)
+			modifle, err := os.OpenFile("./go.mod", os.O_RDONLY, 0755)
+			if err != nil {
+				log.Fatalf("failed to open original modifle: %s\n", err.Error())
+				return
+			}
+			defer modifle.Close()
+
+			copiedModfilePath := fmt.Sprintf("%s/%s", filepath.Dir(glmfile), "go.mod")
+			copiedModfile, err := os.OpenFile(copiedModfilePath, os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0755)
+			if err != nil {
+				// TODO:
+				log.Fatalf("failed to open copied modifle: %s\n", err.Error())
+				// panic(err)
+				return
+			}
+			defer copiedModfile.Close()
+
+			_, err = io.Copy(copiedModfile, modifle)
+			if err != nil {
+				log.Fatalf("failed to copy: %s\n", err.Error())
+				// panic(err)
+				return
+			}
+
+			if err := os.Setenv("MODFILE", copiedModfilePath); err != nil {
+				return
+			}
+			if err := os.Setenv("GO_EXECUTABLE", "go1.14beta1"); err != nil {
+				return
+			}
+
 			output, _ := glm.GetImportablePackages()
 
-			file, err := os.OpenFile(args[0], os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0755)
+			file, err := os.OpenFile(glmfile, os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0755)
 			if err != nil {
 				return
 				// return "", err
 			}
+			defer file.Close()
 
 			fmt.Fprint(file, string(output))
+
+			<-time.After(d)
 		}
 	}()
 
